9e3a8f259b8c466282bcb5e2757974334fabd161
Armando Rojano <armandorour@outlook.com>
2024-10-28 17:36:01 -0600

    Add marquee. Add Tailwind plugin. Other configs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 /**
- * Smoothly scrolls the page to a specified section.
- * @param {string} id - The id of the section to scroll to.
+ * Smoothly scrolls the page to a specified section and updates navigation styles.
+ *
+ * This function scrolls the page to the section with the given ID, adjusts for the header height,
+ * and updates the navigation menu to reflect the active section by adding specific styles.
+ *
+ * @param {string} id - The ID of the section to scroll to.
  */
 function scrollToSection(id) {
   const element = document.getElementById(id);
@@ -12,7 +16,59 @@ function scrollToSection(id) {
     top: offsetPosition,
     behavior: 'smooth',
   });
+
+  const navItems = document.querySelectorAll('nav div[id^="nav-"]');
+  navItems.forEach((item) => {
+    item.classList.remove('line-through', 'text-accent');
+  });
+
+  const activeNavItem = document.getElementById(`nav-${id}`);
+  if (activeNavItem) {
+    activeNavItem.classList.add('line-through', 'text-accent');
+  }
 }
+
+/**
+ * Highlights the navigation item of the currently active section.
+ *
+ * This function loops through all sections on the page and checks which one is currently visible
+ * by comparing the section's top bounding rectangle to the window's inner height. The ID of the
+ * visible section is then used to update the navigation menu styles by adding specific styles
+ * to the corresponding navigation item.
+ *
+ * If the user scrolls to the bottom of the page and no section is visible, no navigation item is
+ * highlighted.
+ */
+function highlightActiveSection() {
+  const sections = document.querySelectorAll('section');
+  const navItems = document.querySelectorAll('nav div[id^="nav-"]');
+
+  let activeSectionId = '';
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top >= 0 && rect.top < window.innerHeight * 0.5) {
+      activeSectionId = section.id; // Guardar la ID de la sección visible
+    }
+  });
+
+  navItems.forEach((item) => {
+    item.classList.remove('line-through', 'text-accent');
+  });
+
+  if (activeSectionId) {
+    const activeNavItem = document.getElementById(`nav-${activeSectionId}`);
+    if (activeNavItem) {
+      activeNavItem.classList.add('line-through', 'text-accent');
+    }
+  }
+}
+
+/**
+ * Adds an event listener to the window's scroll event to trigger the highlightActiveSection function.
+ */
+window.addEventListener('scroll', highlightActiveSection);
 
 /**
  * Smoothly scrolls the page to the top.
@@ -24,6 +80,9 @@ function scrollToTop() {
   });
 }
 
+/**
+ * Copies the email to the clipboard and shows a notification to the user.
+ */
 function copyEmailToClipboard() {
   const email = 'armando_rour@outlook.com';
   navigator.clipboard
@@ -33,6 +92,7 @@ function copyEmailToClipboard() {
       alert.classList.remove('hidden', 'opacity-0');
       alert.classList.add('opacity-100');
 
+      // Hide the alert after 3 seconds
       setTimeout(() => {
         alert.classList.remove('opacity-100');
         alert.classList.add('opacity-0');
@@ -45,3 +105,13 @@ function copyEmailToClipboard() {
       console.error('Error al copiar el correo: ', error);
     });
 }
+
+$(document).ready(function () {
+  $('.marquee').marquee({
+    duration: 40000,
+    startVisible: true,
+    delayBeforeStart: 0,
+    direction: 'left',
+    duplicated: true,
+  });
+});
